f30e893ef4aa5d4f27f1b4e502cf3334714d2223
Sahil <smehar@aceproject.space>
2021-03-31 20:14:15 -0500

    Updated with API_KEY

--- src/request.js
+++ src/request.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 
-console.log(process.env)
 
-const {API_KEY} = process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+
+
 
  const requests ={
      fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
