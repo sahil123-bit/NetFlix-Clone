441b084e86c927011f8e803c4000c792358effa1
Sahil <smehar@aceproject.space>
2021-03-31 20:01:53 -0500

    Row.js is imported

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import './Style/App.css'
+import './Style/App.css';
+import Row from './Row';
+import request from './request';
 
 function App() {
   return (
@@ -8,7 +10,17 @@ function App() {
       {/* Banner => You will se random images of movies*/}
 
       {/* Rows  */}
-      
+      <Row title="NETFLIX ORIGINALS" fetchUrl ={request.fetchNetflixOriginals}
+      isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl ={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl ={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl ={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl ={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl ={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl ={request.fetchRomanceMovies} />
+      <Row title="Documentries" fetchUrl ={request.fetchDocumentries} />
+
     </div>
   );
 }
